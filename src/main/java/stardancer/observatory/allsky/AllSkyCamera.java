2b743a1566af275d154f4916a3eb130ab2589306
michaelg <michael@glibstrup.net>
2018-08-29 21:01:31 +0200

    Got stuff working - veeeery roughly...

--- src/main/java/stardancer/observatory/allsky/AllSkyCamera.java
+++ src/main/java/stardancer/observatory/allsky/AllSkyCamera.java
@@ -1,18 +1,58 @@
 package stardancer.observatory.allsky;
 
-import org.indilib.i4j.*;
+import org.apache.commons.cli.CommandLine;
+import org.apache.log4j.Logger;
+import org.indilib.i4j.Constants;
+import org.indilib.i4j.client.INDIProperty;
+
+import java.util.List;
+import java.util.Map;
 
 public class AllSkyCamera {
 
 
+    private static final Logger LOGGER = Logger.getLogger(AllSkyCamera.class);
+
+    public static void main(String[] args)  throws Exception {
+
+        Logging.setupLogConsoleLogger();
+        CommandLine cmd = CommandLineController.parseCommandLine(args);
+//        Logging.setupFileLogger(cmd);
+
+        String host = cmd.getOptionValue("s", "127.0.0.1");
+        String port = cmd.getOptionValue("p", Integer.toString(Constants.INDI_DEFAULT_PORT));
 
 
-    public static void main() {
-        System.out.println("This is a test");
+        IndiClient indiClient = new IndiClient(host, Integer.parseInt(port));
+
+
+//        new IndiClient(host, Integer.parseInt(port));
+
+//        List<INDIDevice> devices = indiClient.getAllDevices();
+
+        Map<String, Device> test = indiClient.getDevices();
+
+        List<String> deviceNames = indiClient.getDevicesNames();
+
+        String hest  = "";
+
+        Thread.sleep(1000);
+
+        while (true) {
+            if (indiClient.has_change()) {
+                System.out.println("Hest: ");
+                for (String deviceName : indiClient.getDevicesNames()) {
+                    for (INDIProperty hes : indiClient.getDevice(deviceName).getAllProperties()) {
+                        System.out.println(hes.getNameStateAndValuesAsString());
+                    }
+                }
+                System.exit(0);
+            }
+
+        }
+
+
     }
-
-
-
 
 
 }
